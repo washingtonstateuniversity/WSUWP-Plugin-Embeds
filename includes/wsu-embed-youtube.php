211bf23aee13eef105c80bb2b43a2cce542a24a4
Phil Cable <pcable@wsu.edu>
2016-04-15 16:33:34 -0700

    Move function into `includes/wsu-embed-youtube.php`, add documention

--- includes/wsu-embed-youtube.php
+++ includes/wsu-embed-youtube.php
@@ -6,6 +6,9 @@
  * Additional YouTube compat functions forked from Jetpack as of Dec 10, 2015 under GPL v2.0 or later license
  * https://github.com/Automattic/jetpack/blob/master/functions.compat.php
  *
+ * `shortcode_new_to_old_params` function forked from Jetpack as of Apr 15, 2016 under GPL v2.0 or later license
+ * https://github.com/Automattic/jetpack/blob/master/modules/shortcodes.php#L15-L39
+ *
  * Thanks Jetpack!
  */
 
@@ -383,5 +386,31 @@ if ( !function_exists( 'youtube_sanitize_url' ) ) :
 		}
 
 		return $url;
+	}
+endif;
+
+if ( ! function_exists( 'shortcode_new_to_old_params' ) ) :
+	/**
+	 * Transforms the $atts array into a string that the old functions expected
+	 *
+	 * The old way was:
+	 * [shortcode a=1&b=2&c=3] or [shortcode=1]
+	 * This is parsed as array( a => '1&b=2&c=3' ) and array( 0 => '=1' ), which is useless
+	 *
+	 * @param Array $params
+	 * @param Bool $old_format_support true if [shortcode=foo] format is possible.
+	 * @return String $params
+	 */
+	function shortcode_new_to_old_params( $params, $old_format_support = false ) {
+		$str = '';
+		if ( $old_format_support && isset( $params[0] ) ) {
+			$str = ltrim( $params[0], '=' );
+		} elseif ( is_array( $params ) ) {
+			foreach ( array_keys( $params ) as $key ) {
+				if ( ! is_numeric( $key ) )
+					$str = $key . '=' . $params[$key];
+			}
+		}
+		return str_replace( array( '&amp;', '&#038;' ), '&', $str );
 	}
 endif;
--- wsuwp-embeds.php
+++ wsuwp-embeds.php
@@ -8,30 +8,6 @@ Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
 */
 
-/**
- * Transforms the $atts array into a string that the old functions expected
- *
- * The old way was:
- * [shortcode a=1&b=2&c=3] or [shortcode=1]
- * This is parsed as array( a => '1&b=2&c=3' ) and array( 0 => '=1' ), which is useless
- *
- * @param Array $params
- * @param Bool $old_format_support true if [shortcode=foo] format is possible.
- * @return String $params
- */
-function shortcode_new_to_old_params( $params, $old_format_support = false ) {
-	$str = '';
-	if ( $old_format_support && isset( $params[0] ) ) {
-		$str = ltrim( $params[0], '=' );
-	} elseif ( is_array( $params ) ) {
-		foreach ( array_keys( $params ) as $key ) {
-			if ( ! is_numeric( $key ) )
-				$str = $key . '=' . $params[$key];
-		}
-	}
-	return str_replace( array( '&amp;', '&#038;' ), '&', $str );
-}
-
 include_once( __DIR__ . '/includes/wsu-embed-twitter.php' );
 include_once( __DIR__ . '/includes/wsu-embed-uchat.php' );
 include_once( __DIR__ . '/includes/wsu-embed-facebook-post.php' );
