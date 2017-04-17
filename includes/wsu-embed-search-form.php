ca111d7f8cd73cf5228ede5e5b54ed983c8bf9e7
Phil Cable <pcable@wsu.edu>
2017-04-17 09:11:32 -0700

    Allow searching for posts within a specific category or tag

--- includes/wsu-embed-search-form.php
+++ includes/wsu-embed-search-form.php
@@ -6,6 +6,13 @@ class WSUWP_Embed_Search_Form {
 	}
 
 	public function display_wsuwp_search_form( $atts ) {
+		$defaults = array(
+			'site_category_slug' => '',
+			'tag_slug' => '',
+		);
+
+		$atts = shortcode_atts( $defaults, $atts );
+
 		ob_start();
 
 		?>
@@ -13,6 +20,34 @@ class WSUWP_Embed_Search_Form {
 			<div>
 				<label class="screen-reader-text" for="s">Search for:</label>
 				<input type="text" value="" name="s" id="s" />
+				<?php
+				foreach ( $atts as $att => $slug ) {
+
+					if ( '' === $slug ) {
+						continue;
+					}
+
+					if ( 'site_category_slug' === $att ) {
+						$taxonomy = 'category';
+						$parameter = 'category_name';
+					}
+
+					if ( 'tag_slug' === $att ) {
+							$taxonomy = 'post_tag';
+							$parameter = 'tag';
+					}
+
+					$term = get_term_by( 'slug', esc_html( $slug ), $taxonomy );
+
+					if ( $term ) {
+						?>
+						<input type="hidden"
+							   value="<?php echo esc_attr( $slug ); ?>"
+							   name="<?php echo esc_attr( $parameter ); ?>" />
+						<?php
+					}
+				}
+				?>
 				<input type="submit" id="searchsubmit" value="Search" />
 			</div>
 		</form>
