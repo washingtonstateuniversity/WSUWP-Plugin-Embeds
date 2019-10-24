3809d19a92f2891f14da068e8e9ce0d6a9a07b4e
danialbleile <danial.bleile@wsu.edu>
2019-10-24 12:30:36 -0700

    Add support for key & type attrs

--- displays/fatv-embed.php
+++ displays/fatv-embed.php
@@ -7,4 +7,4 @@
  * @var string $playlist_id Playlist ID.
  */
 ?>
-<script type="text/javascript" src="https://embed.financialaidtv.com/cms/embed/script-playlist-ng/1766?iid=<?php echo esc_attr( $playlist_id ); ?>&video=null&responsive=1&language=en"></script>
+<script type="text/javascript" src="https://embed.financialaidtv.com/cms/embed/<?php echo esc_attr( $type ); ?>/<?php echo esc_attr( $playlist_key ); ?>?iid=<?php echo esc_attr( $playlist_id ); ?>&video=null&responsive=1&language=en"></script>
