e1d31afc830193ad7a9480570d2aeb6576b8f37e
Nic Ford <nicdford@gmail.com>
2020-05-21 10:30:14 -0700

    Use img elements instead of photos as backgrounds, include option to change download image size

--- displays/photo-carousel.php
+++ displays/photo-carousel.php
@@ -3,7 +3,7 @@
 /**
  * @var $ids						 array IDs for the images to be displayed in the carousel.
  * @var $name						 string Returns the name of the current instance from the shortcode params or the current page id (limits usage to one per page).
- * @var $image_size					 string Image size identifier, ‘thumb’, ‘thumbnail’, ‘medium’, ‘large’, ‘post-thumbnail’, or any custom image sizes.
+ * @var $image_size					 string Image size identifier. Default medium.
  * @var $slides_per_view			 string Number of slides per view (slides visible at the same time on slider's container).
  * @var $slides_per_column			 string Number of slides per column, for multirow layout.
  * @var $space_between				 string Distance between slides in px.
@@ -11,6 +11,7 @@
  * @var $lazy						 string Enables images lazy loading. If you use slidesPerView, then you should also enable watchSlidesVisibility and Swiper will load images in currently visible slides.
  * @var $watch_slides_visibility	 string Enable this option and slides that are in viewport will have additional visible class.
  * @var $download_image_on_click	 boolean Returns true or false, if the user whats the images to download on click.
+ * @var $download_image_size		 string Image size identifier. Default full.
  */
 ?>
 
@@ -28,9 +29,9 @@
 		font-size: 18px;
 		background: #fff;
 		height: 100%;
+		object-fit: cover;
+		overflow: hidden;
 
-
-		/* Center slide text vertically */
 		display: -webkit-box;
 		display: -ms-flexbox;
 		display: -webkit-flex;
@@ -43,9 +44,6 @@
 		-ms-flex-align: center;
 		-webkit-align-items: center;
 		align-items: center;
-
-		background-size: cover;
-		background-position: 50% 50%;
 	}
 
 
@@ -53,7 +51,7 @@
 		height: calc((100% - <?php echo esc_html($space_between * ($slides_per_column - 1));?>px) / <?php echo esc_html($slides_per_column);?>);
 	}
 
-	#swiper .swiper-slide[data-url]:hover {
+	#swiper [data-download-url]:hover {
 		cursor: pointer;
 	}
 
@@ -105,16 +103,21 @@
 		<?php if ($lazy == 'true') : ?>
 			<?php foreach ($ids as $photo_id) : ?>
 				<?php $image_url = wp_get_attachment_image_src($photo_id, $image_size)[0]; ?>
+				<?php $download_image_url = wp_get_attachment_image_src($photo_id, $download_image_size)[0]; ?>
 
-				<div class="swiper-slide swiper-lazy" data-background="<?php echo esc_url($image_url);?>" <?php if ($download_image_on_click == 'true') : ?> data-url="<?php echo esc_url($image_url);?>" <?php endif; ?>>
+				<div class="swiper-slide">
 					<div class="swiper-lazy-preloader"></div>
+					<img data-src="<?php echo esc_url($image_url);?>" class="swiper-lazy" <?php if ($download_image_on_click == 'true') : ?> data-download-url="<?php echo esc_url($download_image_url);?>" <?php endif; ?>>
 				</div>
 			<?php endforeach; ?>
 		<?php else: ?>
 			<?php foreach ($ids as $photo_id) : ?>
 				<?php $image_url = wp_get_attachment_image_src($photo_id, $image_size)[0]; ?>
+				<?php $download_image_url = wp_get_attachment_image_src($photo_id, $download_image_size)[0]; ?>
 
-				<div class="swiper-slide" style="background-image:url('<?php echo esc_url($image_url);?>');" <?php if ($download_image_on_click == 'true') : ?> data-url="<?php echo esc_url($image_url);?>" <?php endif; ?>></div>
+				<div class="swiper-slide">
+					<img src="<?php echo esc_url($image_url);?>" <?php if ($download_image_on_click == 'true') : ?> data-download-url="<?php echo esc_url($download_image_url);?>" <?php endif; ?>>
+				</div>
 			<?php endforeach; ?>
 		<?php endif; ?>
 
@@ -162,12 +165,16 @@ document.addEventListener("DOMContentLoaded", function() {
 		.catch(e => console.error(e));
 	}
 
-	const swiperSlides = document.querySelectorAll('.swiper-slide');
+	const swiperSlides = document.querySelectorAll('.swiper-slide img');
 
-	swiperSlides.forEach(slide => {
-		slide.addEventListener('click', (e) => {
+	console.log(swiperSlides);
+
+
+	swiperSlides.forEach(slideImage => {
+		slideImage.addEventListener('click', (e) => {
 			e.preventDefault();
-			downloadResource(slide.dataset.url);
+
+			downloadResource(slideImage.dataset.downloadUrl);
 		});
 	});
 
--- includes/wsu-embed-photo-carousel.php
+++ includes/wsu-embed-photo-carousel.php
@@ -30,7 +30,8 @@ class WSUWP_Embed_Photo_Carousel {
 			'preload_images'          => 'false', // JS booleans must be strings to work with this method
 			'lazy'                    => 'true', // JS booleans must be strings to work with this method
 			'watch_slides_visibility' => 'true', // JS booleans must be strings to work with this method
-			'download_image_on_click' => false
+			'download_image_on_click' => false,
+			'download_image_size'     => 'full'
 		);
 
 		$atts = shortcode_atts( $default_atts, $atts );
@@ -51,6 +52,7 @@ class WSUWP_Embed_Photo_Carousel {
 			$lazy = $atts['lazy'];
 			$watch_slides_visibility = $atts['watch_slides_visibility'];
 			$download_image_on_click = $atts['download_image_on_click'];
+			$download_image_size = $atts['download_image_size'];
 
 			//
 			// Process any Vars as needed
