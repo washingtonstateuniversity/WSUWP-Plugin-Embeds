98ebd5acccf8cd3a0b6dcfbad503a706ce28e8db
Nic Ford <nicdford@gmail.com>
2020-09-08 16:06:27 -0700

    Remove absint for flexibility of units

--- includes/wsu-embed-iframes.php
+++ includes/wsu-embed-iframes.php
@@ -81,8 +81,8 @@ class WSUWP_Embed_Iframes {
 				<?php if ( ! empty( $atts['responsive'] ) ) { ?>
 				style="height:100%;left:0;position:absolute;top:0;width:100%;"
 				<?php } else { ?>
-				width="<?php echo esc_attr( absint( $atts['width'] ) ); ?>"
-				height="<?php echo esc_attr( absint( $atts['height'] ) ); ?>"
+				width="<?php echo esc_attr( $atts['width'] ); ?>"
+				height="<?php echo esc_attr( $atts['height'] ); ?>"
 				<?php } ?>
 				title="<?php echo esc_attr( $atts['title'] ); ?>">This is an embedded <a href="https://office.com">Microsoft Office</a> document, powered by <a href="https://office.com/webapps">Office Online</a>.</iframe>
 		<?php
