42a71b11bb41ff211c8d4e864f778b86b241889f
Matthew Davis <matthew.a.davis@wsu.edu>
2022-09-29 11:46:01 -0700

    Hotfix | 1.5.8.2

--- includes/class-wsuwp-embeds.php
+++ includes/class-wsuwp-embeds.php
@@ -7,7 +7,7 @@ class WSUWP_Embeds {
 	 * @var WSUWP_Embeds
 	 */
 	private static $instance;
-	private static $version = '1.5.8.1';
+	private static $version = '1.5.8.2';
 
 	/**
 	 * Maintains and returns the one instance. Initiate hooks when
